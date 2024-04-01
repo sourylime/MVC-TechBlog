32331a9d3b9c079e91a326d0911b394d38f16068
sourylime <145071829+sourylime@users.noreply.github.com>
2024-04-01 14:25:30 -0700

    vfmkvmlkev

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Project = require('./Project');
-const Comment = require('./Comment');
+
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
@@ -15,4 +15,4 @@ Comment.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Project, Comment };
